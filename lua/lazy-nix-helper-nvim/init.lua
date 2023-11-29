a2d0e97159a945fc5eb535a259e4386c9de4b3f6
Brett Gleason <brettmgleason@gmail.com>
2023-11-28 18:45:46 -0600

    add nix-store command to get all installed vim plugins in the current system

--- lua/lazy-nix-helper-nvim/init.lua
+++ lua/lazy-nix-helper-nvim/init.lua
@@ -5,18 +5,29 @@ local plugins = {}
 -- TODO: list nix installed plugins
 -- TODO: map plugin names to nix store paths
 
+-- function M.error(msg)
+--   vim.notify(msg, vim.log.levels.ERROR, { title = "lazy-nix-helper" })
+-- end
+--
+-- TODO: should this be checking nix-store is installed instead?
+function M.nix_is_installed()
+  return vim.fn.executable("nix")
+end
 
 
 function M.populate_plugin_table()
-  -- TODO: create nix command to query either the nix store or nix configuration file for packages prefixed with 'neovim.plugins'
-  local nix_command = ""
-  -- TODO: use io.popen to run the nix command and read the result into a local variable
-  local nix_output = nil
-  -- TODO: parse nix command output into plugin names and nix store paths
-  for line in nix_output do
-    local plugin_name = nil
-    local plugin_path = nil
-    plugins[plugin_name] = plugin_path
+  if M.nix_is_installed then
+    -- NOTE: this will only work for nixos, not packages installed via nix on non-nixos systems
+    local nix_command = "nix-store --query --requisites /run/current-system | grep vimplugin | cut -d- -f3- | sort"
+    local nix_search_handle = io.popen(nix_command)
+    local nix_search_results = nix_search_handle:lines()
+    nix_search_handle:close()
+    -- TODO: parse nix command output into plugin names and nix store paths
+    for line in nix_search_results do
+      local plugin_name = nil
+      local plugin_path = nil
+      plugins[plugin_name] = plugin_path
+    end
   end
 end
 
