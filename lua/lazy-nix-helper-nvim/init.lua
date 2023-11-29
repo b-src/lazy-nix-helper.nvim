cbab6d0404dbe381a58cbb5dfd2931dfa866ac2c
Brett Gleason <brettmgleason@gmail.com>
2023-11-29 17:56:06 -0600

    parse vim plugin names from nix store paths and populate the plugin table

--- lua/lazy-nix-helper-nvim/init.lua
+++ lua/lazy-nix-helper-nvim/init.lua
@@ -2,30 +2,36 @@ local M = {}
 
 local plugins = {}
 
--- TODO: list nix installed plugins
--- TODO: map plugin names to nix store paths
-
--- function M.error(msg)
---   vim.notify(msg, vim.log.levels.ERROR, { title = "lazy-nix-helper" })
--- end
 --
 -- TODO: should this be checking nix-store is installed instead?
 function M.nix_is_installed()
   return vim.fn.executable("nix")
 end
 
+function M.parse_plugin_name_from_nix_store_path(path)
+  -- path looks like:
+  -- /nix/store/<hash>-vimplugin-<name>[-<date>]
+  -- use string.match to capture everything after "vimplugin-"
+  local plugin_name_with_possible_date = string.match(path, ".vimplugin%-(.*)")
+  -- date is formatted -YYYY-DD-YY. capture on digits fitting that format at the end of the string
+  local plugin_name = string.match(plugin_name_with_possible_date, "(.-)%-%d%d%d%d%-%d%d%-%d%d$")
+  if plugin_name == nil then
+    plugin_name == plugin_name_with_possible_date
+  end
+  return plugin_name
+end
 
 function M.populate_plugin_table()
   if M.nix_is_installed then
     -- NOTE: this will only work for nixos, not packages installed via nix on non-nixos systems
-    local nix_command = "nix-store --query --requisites /run/current-system | grep vimplugin | cut -d- -f3- | sort"
+    local nix_command = "nix-store --query --requisites /run/current-system | grep vimplugin | sort"
     local nix_search_handle = io.popen(nix_command)
     local nix_search_results = nix_search_handle:lines()
     nix_search_handle:close()
-    -- TODO: parse nix command output into plugin names and nix store paths
     for line in nix_search_results do
-      local plugin_name = nil
-      local plugin_path = nil
+      print(line)
+      local plugin_path = line
+      local plugin_name = M.parse_plugin_name_from_nix_store_path(path)
       plugins[plugin_name] = plugin_path
     end
   end
