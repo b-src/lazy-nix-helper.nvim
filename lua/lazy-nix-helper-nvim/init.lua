499ed02c250da8f32b1a2158893e1d9f4d0e4577
Brett Gleason <brettmgleason@gmail.com>
2023-11-29 19:35:40 -0600

    tweaks and fixes so that current functionality works. be clear about what functions are part of the public api. populate_plugin_table() currently leaves a dangling file handle, that will need to be addressed

--- lua/lazy-nix-helper-nvim/init.lua
+++ lua/lazy-nix-helper-nvim/init.lua
@@ -2,13 +2,16 @@ local M = {}
 
 local plugins = {}
 
---
+function error(msg)
+  vim.notify(msg, vim.log.levels.ERROR, { title = "lazy-nix-helper" })
+end
+
 -- TODO: should this be checking nix-store is installed instead?
-function M.nix_is_installed()
+function nix_is_installed()
   return vim.fn.executable("nix")
 end
 
-function M.parse_plugin_name_from_nix_store_path(path)
+function parse_plugin_name_from_nix_store_path(path)
   -- path looks like:
   -- /nix/store/<hash>-vimplugin-<name>[-<date>]
   -- use string.match to capture everything after "vimplugin-"
@@ -16,29 +19,32 @@ function M.parse_plugin_name_from_nix_store_path(path)
   -- date is formatted -YYYY-DD-YY. capture on digits fitting that format at the end of the string
   local plugin_name = string.match(plugin_name_with_possible_date, "(.-)%-%d%d%d%d%-%d%d%-%d%d$")
   if plugin_name == nil then
-    plugin_name == plugin_name_with_possible_date
+    plugin_name = plugin_name_with_possible_date
   end
   return plugin_name
 end
 
 function M.populate_plugin_table()
-  if M.nix_is_installed then
+  if nix_is_installed then
     -- NOTE: this will only work for nixos, not packages installed via nix on non-nixos systems
     local nix_command = "nix-store --query --requisites /run/current-system | grep vimplugin | sort"
     local nix_search_handle = io.popen(nix_command)
     local nix_search_results = nix_search_handle:lines()
-    nix_search_handle:close()
+    -- TODO: this is needed, we don't want to leave dangling file handles for lua's GC to clean up
+    -- however, when this is included, attempting to use nix_search_results below gives an error:
+    -- "attempt to use a closed file"
+    -- I'm not sure why that is.
+    -- nix_search_handle:close()
     for line in nix_search_results do
-      print(line)
       local plugin_path = line
-      local plugin_name = M.parse_plugin_name_from_nix_store_path(path)
+      local plugin_name = parse_plugin_name_from_nix_store_path(line)
       plugins[plugin_name] = plugin_path
     end
   end
 end
 
 
-function M.tableContains(table, key)
+function table_contains(table, key)
   return table[key] ~= nil
 end
 
@@ -46,8 +52,7 @@ function M.get_plugin_path(plugin_name)
   if not plugin_name then
     error("plugin_name not provided")
   end
-  if not M.tableContains(plugins, plugin_name) then
-    local message = 
+  if not table_contains(plugins, plugin_name) then
     error("lazy-nix-helper plugin table does not contain " .. plugin_name)
   end
   return plugins[plugin_name]
