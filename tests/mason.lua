088263802b7524bc168d959d05971c3ac1a0ef64
Brett Gleason <brettmgleason@gmail.com>
2023-12-04 19:52:14 -0600

    figure out how to use plenary for testing and add tests for mason module. TODO: make sure test setup also works on non-nix environments

---
+++ tests/mason.lua
@@ -0,0 +1,32 @@
+local Core = require("lazy-nix-helper.core")
+local Mason = require("lazy-nix-helper.mason")
+local Util = require("lazy-nix-helper.util")
+
+local plenary_path = Core.get_plugin_path("plenary.nvim")
+print("plenary path: " .. plenary_path)
+vim.opt.rtp:prepend(plenary_path)
+
+local plenary = require("plenary")
+local mock = require("luassert.mock")
+local assert = require("luassert.assert")
+
+
+plenary.busted.describe("test mason_enabled", function()
+  local tests = {
+    { true, false },
+    { false, true },
+  }
+
+  for _, test in ipairs(tests) do
+    plenary.busted.it("returns correct value when in_a_nix_environment returns " .. tostring(test[1]), function()
+      local Util = mock(Util, true)
+      Util.in_a_nix_environment.returns(test[1])
+
+      local result = Mason.mason_enabled()
+
+      assert.equals(result, test[2])
+
+      mock.revert(Util)
+    end)
+  end
+end)
