1b32cd445fb9cb68f4e3904443defbbb91fe906a
Brett Gleason <brettmgleason@gmail.com>
2023-12-04 20:44:00 -0600

    add config tests

---
+++ tests/config.lua
@@ -0,0 +1,30 @@
+local Config = require("lazy-nix-helper.config")
+local Util = require("lazy-nix-helper.util")
+
+require("tests.init").setup_test_env()
+
+local plenary = require("plenary")
+local mock = require("luassert.mock")
+local assert = require("luassert.assert")
+
+
+plenary.busted.describe("test setup", function()
+  plenary.busted.it("throws an error if no lazypath provided", function()
+      local Util = mock(Util, true)
+
+      Config.setup({})
+
+      assert.stub(Util.error).was_called_with("A default lazypath must be provided")
+
+      mock.revert(Util)
+  end)
+end)
+
+plenary.busted.describe("test setup", function()
+  plenary.busted.it("sets lazypath", function()
+    local expected_result = "test/lazypath"
+    Config.setup({ lazypath = expected_result })
+
+    assert.equals(Config.options.lazypath, expected_result)
+  end)
+end)
