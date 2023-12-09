5a61b32a40cbe6fd31dd5a8d0aa33194643649ac
Brett Gleason <brettmgleason@gmail.com>
2023-12-09 12:23:23 -0600

    wip update init file to install dependencies locally. add test_dependencies dir to .gitignore

--- tests/init.lua
+++ tests/init.lua
@@ -1,21 +1,30 @@
 M = {}
 
-function M.setup_test_env()
+local function repo_path()
   local file_path = debug.getinfo(1, "S").source:sub(2)
-  local lazy_nix_helper_absolute_path = string.match(file_path, "(.*).tests/init%.lua")
+  local full_file_path = vim.fn.fnamemodify(file_path, ":p")
+  local lazy_nix_helper_absolute_path = string.match(full_file_path, "(.*).tests/init%.lua") .. "/"
+  return lazy_nix_helper_absolute_path
+end
 
-  vim.opt.rtp:prepend(lazy_nix_helper_absolute_path)
+local function install_deps()
+  local repo_root = repo_path()
+  local dependency_root = repo_root .. "test_dependencies/"
+  local plenary_path = dependency_root .. "plenary/"
+
+  vim.fn.mkdir(dependency_root, "p")
+  vim.fn.system({ "git", "clone", "--depth=1", "https://github.com/nvim-lua/plenary.nvim.git", plenary_path })
+
+  vim.opt.rtp:prepend(plenary_path)
+end
+
+function M.setup_test_env()
+  vim.opt.rtp:prepend(repo_path())
+
+  install_deps()
 
   local non_nix_lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
   require("lazy-nix-helper").setup({ lazypath = non_nix_lazypath })
-
-
-  local plenary_path = require("lazy-nix-helper").get_plugin_path("plenary.nvim")
-  -- TODO: can't assume plenary will be available here or at all
-  if plenary_path == nil then
-    plenary_path = vim.fn.stdpath("data") .. "/lazy/plenary.nvim"
-  end
-  vim.opt.rtp:prepend(plenary_path)
 end
 
 M.setup_test_env()
