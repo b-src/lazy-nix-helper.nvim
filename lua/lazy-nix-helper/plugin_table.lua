00afa204349723c9f3a4bef5f24d455fb9c486b9
Brett Gleason <brettmgleason@gmail.com>
2023-12-29 12:24:53 -0600

    add tests for plugin_table module

--- lua/lazy-nix-helper/plugin_table.lua
+++ lua/lazy-nix-helper/plugin_table.lua
@@ -5,7 +5,7 @@ local M = {}
 M.plugins = {}
 
 function M.populate_provided_plugin_paths(input_plugin_table)
-  for key, value in ipairs(input_plugin_table) do
+  for key, value in pairs(input_plugin_table) do
     if Util.file_exists(value) then
       M.plugins[key] = value
     end
